995f96e5025a31ba3d6bfb2883de5972c6027209
feroswin <vitalkaivanov009@mail.ru>
2023-05-30 23:17:10 +0300

    replace js btnHint

--- js/index.js
+++ js/index.js
@@ -1,27 +1,23 @@
+function dropdownToggleActive(index) {
+    dropdownItem[index].classList.toggle("active")
+    dropdownBody[index].classList.toggle("active")
+}
+
+function changeContentBtnHint(contentElement) {
+    return (contentElement === "Показать") ? "Скрыть" : "Показать"
+}
 
 
 let dropdownItem = document.querySelectorAll(".dropdown-item")
 let dropdownBody = document.querySelectorAll(".dropdown-body")
-
-
 dropdownItem.forEach((elem, i) => {
-    elem.addEventListener("mouseover", () => {
-        dropdownItem[i].classList.toggle("active")
-        dropdownBody[i].classList.toggle("active")
-    })
-
-    elem.addEventListener("mouseout", () => {
-        dropdownItem[i].classList.toggle("active")
-        dropdownBody[i].classList.toggle("active")
-    })
+    elem.addEventListener("mouseover", () => dropdownToggleActive(i))
+    elem.addEventListener("mouseout", () => dropdownToggleActive(i))
 })
-//
-// dropdownItem.addEventListener("mouseover", () => {
-//     dropdownItem.classList.toggle("active")
-//     dropdownBody.classList.toggle("active")
-// })
-//
-// dropdownItem.addEventListener("mouseout", () => {
-//     dropdownItem.classList.toggle("active")
-//     dropdownBody.classList.toggle("active")
-// })
+
+let btnHint = document.querySelector(".btn-hint")
+let hintProfile = document.querySelector(".hint-profile-fullness")
+btnHint.addEventListener("click", () => {
+    btnHint.innerHTML = changeContentBtnHint(btnHint.innerHTML)
+    hintProfile.classList.toggle("active");
+})
