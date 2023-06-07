30e16357dc14ecaa1063d12f917b9ccd83e5c464
feroswin <vitalkaivanov009@mail.ru>
2023-06-07 21:14:03 +0300

    фикс js

--- js/manufacture-orderer-order.js
+++ js/manufacture-orderer-order.js
@@ -3,7 +3,7 @@ let readContinue = document.querySelector(".read-continue")
 let descriptionContent = document.querySelector(".description-content")
 let hidingBlock = document.querySelector(".hiding-block")
 
-readContinue.addEventListener("click", (event) => {
+readContinue.addEventListener("click", () => {
     (readContinue.innerText === "Читать далее") ? readContinue.innerText = "Скрыть" : readContinue.innerText = "Читать далее";
     readContinue.classList.toggle("active")
     descriptionContent.classList.toggle("open")
