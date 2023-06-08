fcd5796dab3fe2f1ac47f3b05e1c9073e217c597
feroswin <vitalkaivanov009@mail.ru>
2023-06-09 00:08:26 +0300

    добавлены модальные окна для закрытия заказа и редактирования, начата модалка с переносом встречи

--- js/manufacture-orderer-order.js
+++ js/manufacture-orderer-order.js
@@ -1,8 +1,8 @@
 
+// Раскрытие блока описания заказа
 let readContinue = document.querySelector(".read-continue")
 let descriptionContent = document.querySelector(".description-content")
 let hidingBlock = document.querySelector(".hiding-block")
-
 readContinue.addEventListener("click", () => {
     (readContinue.innerText === "Читать далее") ? readContinue.innerText = "Скрыть" : readContinue.innerText = "Читать далее";
     readContinue.classList.toggle("active")
@@ -10,12 +10,78 @@ readContinue.addEventListener("click", () => {
     hidingBlock.classList.toggle("hide")
 })
 
-let filename = document.querySelectorAll(".filename")
-filename.forEach((elem,i) => {
+// Реализация прокрутки имени файла
+let file = document.querySelectorAll(".file")
+file.forEach((elem,i) => {
     elem.addEventListener("mouseover", (event) => {
-        filename[i].scroll(0, filename[i].scrollHeight)
+        file[i].children[1].scroll(0, file[i].children[1].scrollHeight)
     })
     elem.addEventListener("mouseout", () => {
-        filename[i].scroll(0, 0)
+        file[i].children[1].scroll(0, 0)
     })
 })
+
+
+
+let btnCloseOrder = document.getElementById("close-order")
+let btnEditOrder = document.getElementById("edit-order")
+let overlay = document.querySelector(".overlay")
+let modalCloseOrder = document.querySelector(".modal-close-order")
+let modalEditOrder = document.querySelector(".modal-edit-order")
+let crossModal = document.querySelectorAll(".cross-modal")
+let undoAction = document.querySelectorAll(".undo-action")
+
+// Открытие модального окна "Закрытие заказа"
+btnCloseOrder.addEventListener("click", () => {
+    openModal(modalCloseOrder)
+})
+
+// Открытие модального окна "Редактирование заказа"
+btnEditOrder.addEventListener("click", ()=> {
+    openModal(modalEditOrder)
+})
+
+// Закрытие конктретного модального окна по кнопке
+undoAction.forEach((elem, i) => {
+    elem.addEventListener("click", () => {
+        closeModal(elem.parentNode.parentNode.parentNode)
+    })
+})
+// Закрытие модального окна по нажатию на крестик
+crossModal.forEach((elem, i) => {
+    elem.addEventListener("click", (event) => {
+        closeModal(elem.parentNode.parentNode)
+    })
+})
+
+
+
+// Заготовка под AJAX для модального окна "Закрытие заказа" при продтверждении действия
+let applyCloseOrder = document.getElementById("apply-close-order")
+applyCloseOrder.addEventListener("click", async () => {
+    await fetch("http://localhost/test/api", {
+        method: "POST"
+    }).then(res => console.log(res)).catch(err => console.log(err))
+    closeModal(modalCloseOrder)
+})
+
+// Заготовка под AJAX для модального окна "Редактирование заказа" при подтвержедении действия
+let applyEditOrder = document.getElementById("apply-edit-order")
+applyEditOrder.addEventListener("click", async () => {
+    await fetch("http://localhost/test/api", {
+        method: "POST"
+    }).then(res => console.log(res)).catch(err => console.log(err))
+    closeModal(modalEditOrder)
+})
+
+
+function closeModal(modal) {
+    modal.classList.remove("active")
+    overlay.classList.remove("active")
+}
+
+
+function openModal(modal) {
+    overlay.classList.add("active")
+    modal.classList.add("active")
+}
